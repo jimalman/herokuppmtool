f4c5f5cdaefbbcd338624dac9836267b0e5a1a59
Jim <jcalman9@gmail.com>
2019-02-17 19:11:53 -0500

    added new files for js build

---
+++ src/main/resources/static/precache-manifest.7683e771721fb6847615e27092151506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c5a7e3595f584a06972c",
+    "url": "/static/js/main.c5a7e359.chunk.js"
+  },
+  {
+    "revision": "154970f9d6f96cd7bc3a",
+    "url": "/static/js/1.154970f9.chunk.js"
+  },
+  {
+    "revision": "c5a7e3595f584a06972c",
+    "url": "/static/css/main.3cd79716.chunk.css"
+  },
+  {
+    "revision": "154970f9d6f96cd7bc3a",
+    "url": "/static/css/1.b1b6aede.chunk.css"
+  },
+  {
+    "revision": "aff39d80a97a5a9c4fdbd6097fb19d3e",
+    "url": "/index.html"
+  }
+];
